f6ea9b6e72a3fa81e811de6e305887958051dfc2
Ruan Montelo <ruanmontelo@gmail.com>
2020-08-07 18:53:01 -0300

    Fixing version route by creating it first, if not express cant read the object correctly

--- src/routes.ts
+++ src/routes.ts
@@ -6,8 +6,9 @@ const versionController = new VersionController();
 const urlController = new URLController();
 const routes = Router();
 
-routes.get('/:urlId', urlController.get);
 routes.get('/version', versionController.get);
+
+routes.get('/:urlId', urlController.get);
 routes.post('/url', urlController.post);
 
 export default routes;
