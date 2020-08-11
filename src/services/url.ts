4de2ff60755181a04989994400bda98fbadda812
Ruan Montelo <ruanmontelo@gmail.com>
2020-08-10 23:56:51 -0300

    Creating instances of the Url (database) class in the methods of the services file to stay only inside of the scope of that method, since that will be more important later

--- src/services/url.ts
+++ src/services/url.ts
@@ -1,14 +1,14 @@
 import Url from '../database/url'
 import IUrlResponse from '../validators/interfaces/IUrlResponse'
 
-const urlInterfaceToDatabase = new Url()
-
 export default class UrlServices {
     public async getURL(uuid: string): Promise<IUrlResponse> {
+        const urlInterfaceToDatabase = new Url()
         return await urlInterfaceToDatabase.getURLByUuid(uuid)
     }
 
     public async createURL(url: string): Promise<IUrlResponse> {
+        const urlInterfaceToDatabase = new Url()
         return await urlInterfaceToDatabase.writeURL(url);
     }
 }
