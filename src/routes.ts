f9259d8bfb3b810a2614379285cbe72112d6a33b
Ruan Montelo <ruanmontelo@gmail.com>
2020-08-17 00:01:35 -0300

    Removing the semi-colons from the routes file to maintain the pattern

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import VersionController from './controllers/version'
 import URLController from './controllers/url'
 
-const versionController = new VersionController();
-const urlController = new URLController();
-const routes = Router();
+const versionController = new VersionController()
+const urlController = new URLController()
+const routes = Router()
 
-routes.get('/version', versionController.get);
+routes.get('/version', versionController.get)
 
-routes.get('/:uuid', urlController.get);
-routes.post('/url', urlController.post);
+routes.get('/:uuid', urlController.get)
+routes.post('/url', urlController.post)
 
-export default routes;
+export default routes
