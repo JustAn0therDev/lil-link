bacccc6a31911f2435fd21e0dce36089788783dd
Ruan Montelo <ruanmontelo@gmail.com>
2020-08-26 22:18:22 -0300

    Changing order of declarations in the routes.ts file

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import URLController from './controllers/url'
 import VersionController from './controllers/version'
 
-const versionController = new VersionController()
-const urlController = new URLController()
 const routes = Router()
+const urlController = new URLController()
+const versionController = new VersionController()
 
 routes.get('/version', versionController.get)
-
 routes.get('/:uuid', urlController.get)
+
 routes.post('/url', urlController.post)
 
 export default routes
